8d6dabe595056e49c0d08e7b5042794d4a6039d5
Jorge Villegas <jorgevillegas77@gmail.com>
2023-03-10 15:45:37 -0800

    first commit

---
+++ backend/config/plugins.tsx
@@ -0,0 +1,15 @@
+export default {
+  //
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
